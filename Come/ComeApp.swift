31798d7708c77632b68365620120349203b0bf58
Ariana Nagumo <arinagumo@gmail.com>
2023-10-31 18:40:27 -0600

    Initial Commit

---
+++ Come/ComeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComeApp.swift
+//  Come
+//
+//  Created by Ariana Santillán on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ComeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
